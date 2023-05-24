470904db1c504d5214917d954ae439e4e52b59a9
Nenad Prahovljanovic <nenad.prahovljanovic@dev22.rs>
2023-05-24 19:58:47 +0200

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+    name: "Fi22",
+    platforms: [
+        .iOS(.v13),
+        .tvOS(.v13),
+        .macOS(.v10_15),
+        .watchOS(.v6)
+    ],
+    products: [
+        .library(name: "Fi22", targets: ["Fi22"]),
+    ],
+    targets: [
+        .target(name: "Fi22"),
+    ]
+)
